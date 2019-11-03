9363636a45b1829c88d2ce89a0f46b0cccef8242
ptrckr <github@pr.link>
2019-11-03 19:21:41 +0100

    Added project `GeishaBoy.tf'

--- content/src/project/geishaboy/config.js
+++ content/src/project/geishaboy/config.js
@@ -2,9 +2,30 @@ module.exports = {
   name: "GeishaBoy.tf",
   description: "A site featuring my collection of the item \"Geisha Boy\" from the video game Team Fortress 2.",
   keywords: ["Website", "Team Fortress 2"],
-  created: "MAR 2018",
+  created: "NOV 2019",
 
-  extern_url: "https://geishaboy.tf",
+  display_on_frontpage: true,
 
-  display_on_frontpage: true
+  pug_data: {
+    get collection() {
+      let meta = require("./js/geisha_data.js");
+
+      meta.stats = {
+        collected: 0,
+        total: 0
+      };
+
+      meta.series.forEach(update => {
+        update.stats = {
+          collected: update.effects.filter(e => e.collected).length,
+          total: update.effects.length
+        }
+
+        meta.stats.collected += update.stats.collected;
+        meta.stats.total += update.stats.total;
+      });
+
+      return meta;
+    }
+  }
 }
