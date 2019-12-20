c2e4f2df6187fa498529b29eb54dc804f7fe0934
ptrckr <github@pr.link>
2019-12-20 19:42:13 +0100

    Removed experimental folder

--- content/src/experimental/input-snake/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: "Input Snake",
-  description: "A working clone of snake with the HTML Input Element",
-  keywords: ["Web Experiment"],
-  created: "AUG 2019"
-}
--- content/src/experimental/input-snake/js/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-import InputSnake from "./input_snake.js";
-
-const input_snake = new InputSnake(
-  document.querySelector(".game")
-);
